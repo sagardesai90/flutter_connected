8b42a1773813e3ca951e34d0a78144fb08fcfd6a
sagardesai90 <saagardesai@gmail.com>
2018-12-18 16:16:18 -0600

    navigation drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,141 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+const String _AccountName = 'Sagar Desai';
+const String _AccountEmail = 'saagardesai@gmail.com';
+const String _AccountAbbr = 'BMW';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+void main() => runApp(new Keeper());
+
+class Keeper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return new MaterialApp(
+      title: 'BMW Connected',
+      theme: new ThemeData(
+        primaryColor: Colors.grey.shade900
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new KeeperDrawer(),		
+    );
+  }	
+}
+
+
+class KeeperDrawer extends StatelessWidget {
+// refer: https://docs.flutter.io/flutter/widgets/ListView-class.html
+// refer: https://docs.flutter.io/flutter/widgets/GestureDetector-class.html
+// refer: https://docs.flutter.io/flutter/material/ListTile-class.html
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      drawer: new Drawer(
+        child: new ListView(
+          padding: const EdgeInsets.only(top: 0.0),
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+              accountName: const Text(_AccountName),
+              accountEmail: const Text(_AccountEmail),
+              currentAccountPicture: new CircleAvatar(
+                backgroundColor: Colors.brown,
+                child: new Text(_AccountAbbr)
+              ),
+              otherAccountsPictures: <Widget>[
+                new GestureDetector(
+                  onTap: () => _onTapOtherAccounts(context),
+                  child: new Semantics(
+                    label: 'Switch Account',
+                    child: new CircleAvatar(
+                      backgroundColor: Colors.brown,
+                      child: new Text('SA'),
+                    ),
+                  ),
+                )
+	            ]
+            ),
+            new ListTile(
+              leading: new Icon(Icons.directions_car),
+              title: new Text('My Vehicle'),
+              onTap: () => _onListTileTap(context),
+            ),
+            new Divider(),
+            new ListTile(
+              leading: new Text('Activity'),
+              trailing: new Text('Edit'),
+              onTap: () => _onListTileTap(context),
+            ),
+            new ListTile(
+              leading: new Icon(Icons.transform),
+              title: new Text('Destinations'),
+              onTap: () => _onListTileTap(context),
+            ),
+            new ListTile(
+              leading: new Icon(Icons.apps),
+              title: new Text('Hub'),
+              onTap: () => _onListTileTap(context),
+            ),
+            new ListTile(
+              leading: new Icon(Icons.person),
+              title: new Text('Profile'),
+              onTap: () => _onListTileTap(context),
+            ),
+            new Divider(),
+            
+            
+            new ListTile(
+              leading: new Icon(Icons.settings),
+              title: new Text('Settings'),
+              onTap: () => _onListTileTap(context),
+            ),
+            new ListTile(
+              leading: new Icon(Icons.account_balance),
+              title: new Text('Legal'),
+              onTap: () => _onListTileTap(context),
+            ),
+          ]
+        )
+      ),
+      appBar: new AppBar(
+        title: new Text('BMW Connected'),
+      ),
+      body: new Center(
+        child: new Text('Hello World!')
+      )
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  _onTapOtherAccounts(BuildContext context) {
+    Navigator.of(context).pop();
+    showDialog<Null>(
+      context: context,
+      child: new AlertDialog(
+        title: const Text('Account switching not implemented.'),
+        actions: <Widget>[
+          new FlatButton(
+            child: const Text('OK'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      ),
+    );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+  _onListTileTap(BuildContext context) {
+    Navigator.of(context).pop();
+    showDialog<Null>(
+      context: context,
+      child: new AlertDialog(
+        title: const Text('Not Implemented'),
+        actions: <Widget>[
+          new FlatButton(
+            child: const Text('OK'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
